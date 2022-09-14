19836637bc475c2c6c8598ae8bf503da4127de86
githeou <tss9891@students.uc-bcf.edu.ph>
2022-09-14 14:51:51 +0800

    Auto upload files

--- Auto upload.bat
+++ Auto upload.bat
@@ -1,7 +1,7 @@
 @echo off
 git config --global user.email "tss9891@students.uc-bcf.edu.ph"
 git config --global user.name "githeou"
-git remote set-url origin https://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@github.com/githeou/CIT142G.git
+git remote set-url origin https://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@github.com/githeou/CIT142G.git
 git add .
 git commit -m "Auto upload files"
 git push
